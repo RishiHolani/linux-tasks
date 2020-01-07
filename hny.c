da6a1f00deb5dab4c42da988fd7887120667c856
Rishi Holani <47501615+RishiHolani@users.noreply.github.com>
2020-01-07 15:35:09 -0500

    Add files via upload

--- hny.c
+++ hny.c
@@ -8,19 +8,11 @@ MODULE_AUTHOR("Rishi Holani");
 int bin=0;
 module_param(bin, int, S_IRUSR | S_IWUSR);
 
-void main()
-{
-if(bin = 1)
-{   
-	module_init(hny_init);	
-	module_exit(hny_cleanup);
-}
-}
-
 int __init hny_init(void)
 {
-	printk(KERN_INFO "Hello \n");
-	return 0;   
+	if(bin == 1)
+{   printk(KERN_INFO "Hello \n");
+}	return 0;   
 }
 
 void __exit hny_cleanup(void)
